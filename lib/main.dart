c2f62c7baecbecb7bc94be073c50af5ba2c50c8d
Dino Iván Pérez Vásquez <dperez@csticorp.biz>
2023-09-23 20:12:49 -0500

    Proyecto_base

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'data_class.dart';
 
 void main() {
-
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (_)=>DataClass()),
-
-  ],
-      child:const MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,96 +10,60 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const ProviderDemoScreen(),
-
-      );
-  }
-}
-
-class ProviderDemoScreen extends StatefulWidget {
-  const ProviderDemoScreen({Key? key}) : super(key: key);
-
-  @override
-  _ProviderDemoScreenState createState() => _ProviderDemoScreenState();
-}
-
-class _ProviderDemoScreenState extends State<ProviderDemoScreen> {
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:<Widget> [
-              Text(
-                'Bienvenida',
-                style: TextStyle(color: Colors.blueAccent,fontSize: 40.0,
-                    fontWeight: FontWeight.bold),
-                textAlign: TextAlign.start,
-              ),
-              SizedBox(height: 10),
-              Image(
-                image: AssetImage("assets/seguridad.png"),
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: 300, // Ancho igual al ancho de la pantalla
-                child: FloatingActionButton.extended(
-                  label: Text('Empezar'),
-                  backgroundColor: Colors.blueAccent,
-                  splashColor: Colors.greenAccent,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)
-                  ),
-                  onPressed: () {
-
-                  },
-                ),
-              ),
-              SizedBox(height: 20),
-              RichText(text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: '¿Ya tienes una cuenta?',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                      fontFamily: 'Karla',
-                      color: Colors.blueAccent,
-                    ),
-                  ),
-                ],
-              ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: 300, // Ancho igual al ancho de la pantalla
-                child: FloatingActionButton.extended(
-                  label: Text('Iniciar Sesión'),
-                  backgroundColor: Colors.blueAccent,
-                  splashColor: Colors.greenAccent,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)
-                  ),
-                  onPressed: () {
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
